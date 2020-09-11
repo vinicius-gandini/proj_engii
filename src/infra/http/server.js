1343e2e25411d5f2d0af5fc1434d3332866b0b07
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-09-11 20:56:57 -0300

    chore: config eslint and prettier :books:

--- src/infra/http/server.js
+++ src/infra/http/server.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
+
 const server = express();
 
 const routes = require('./routes');
+
 server.use(express.json());
 server.use(routes);
 
-server.listen(3333, function(){
-    console.log("Hello World");
+server.listen(3333, () => {
+  console.log('Hello World');
 });
