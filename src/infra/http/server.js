053eb7f8729e6e38f0866dc51567df2ed14eb86b
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-09-11 22:02:54 -0300

    chore: config husky and lint-staged :books:

--- src/infra/http/server.js
+++ src/infra/http/server.js
@@ -8,5 +8,6 @@ server.use(express.json());
 server.use(routes);
 
 server.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('Hello World');
 });
--- src/modules/users/repositories/UsersRepository.js
+++ src/modules/users/repositories/UsersRepository.js
@@ -1,28 +1,27 @@
 const mongo = require('../../../infra/database/mongoose');
 
 class UsersRepository {
-    async add(data){
+  async add(data) {
+    const user = await mongo
+      .collection('users')
+      .insert(data)
+      .then(result => {
+        return result;
+      });
 
-        const user = await mongo
-        .collection('users')
-        .insert(data)
-        .then(result => {
-            return result;
-        });
+    return user;
+  }
 
-        return user;
-    }
+  async findByEmail(email) {
+    const user = await mongo
+      .collection('users')
+      .findOne({ email })
+      .then(result => {
+        return result;
+      });
 
-    async findByEmail(email) {
-        const user = await mongo
-        .collection('users')
-        .findOne({email})
-        .then(result => {
-            return result;
-        })
-
-        return user;
-    }
+    return user;
+  }
 }
 
 module.exports = new UsersRepository();
